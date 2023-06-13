db2c9c66a59f72f02af3e78f46f69f5a0539c7cb
Sebastian Zarate <129194478+SZarate01@users.noreply.github.com>
2023-06-13 10:03:55 -0500

    nuevos productos dentro de PC, aplican lo mismo para Play, xbox, etc.

--- productosPC.js
+++ productosPC.js
@@ -13,33 +13,78 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
   });
-
+  
   function showProducts(category) {
     var products = [];
 
     if (category === 'pc') {
       products = [
+        { image: 'tarjetas_RAM.png', text: 'Tarjetas RAM' },
+        { image: 'ventiladores.png', text: 'Ventiladores' },
+        { image: 'juegosPC.png', text: 'Juegos' },
         { image: 'perifericos.png', text: 'Periféricos' },
         { image: 'tarjeta_grafica.png', text: 'Tarjetas Gráficas' },
-        { image: 'tarjetas_madre.jpg', text: 'Tarjetas Madre' }
+        { image: 'tarjeta_madre.png', text: 'Tarjetas Madre' }
+      ];
+    } else if (category === 'playstation') {
+      products = [
+        { image: 'producto_ps1.png', text: 'Producto PlayStation 1' },
+        { image: 'producto_ps2.png', text: 'Producto PlayStation 2' },
+        { image: 'producto_ps3.png', text: 'Producto PlayStation 3' }
+      ];
+    } else if (category === 'xbox') {
+      products = [
+        { image: 'producto_xbox1.png', text: 'Producto Xbox 1' },
+        { image: 'producto_xbox2.png', text: 'Producto Xbox 2' },
+        { image: 'producto_xbox3.png', text: 'Producto Xbox 3' }
+      ];
+    } else if (category === 'nintendo') {
+      products = [
+        { image: 'producto_nintendo1.png', text: 'Producto Nintendo 1' },
+        { image: 'producto_nintendo2.png', text: 'Producto Nintendo 2' },
+        { image: 'producto_nintendo3.png', text: 'Producto Nintendo 3' }
+      ];
+    } else if (category === 'mercancia') {
+      products = [
+        { image: 'producto_mercancia1.png', text: 'Producto de Mercancía 1' },
+        { image: 'producto_mercancia2.png', text: 'Producto de Mercancía 2' },
+        { image: 'producto_mercancia3.png', text: 'Producto de Mercancía 3' }
       ];
     }
 
     productContainer.innerHTML = '';
 
-    products.forEach(function(product) {
+    var row = document.createElement('div');
+    row.className = 'product-row';
+
+    for (var i = 0; i < products.length; i++) {
       var productBox = document.createElement('div');
       productBox.className = 'product-box';
 
       var productImage = document.createElement('img');
-      productImage.src = product.image;
+      productImage.src = products[i].image;
 
       var productText = document.createElement('div');
-      productText.textContent = product.text;
+      productText.textContent = products[i].text;
+
+      if (i < 3) {
+        row.appendChild(productBox);
+      } else {
+        var rowIndex = Math.floor(i / 3);
+        var rowContainer = productContainer.getElementsByClassName('product-row')[rowIndex];
+        if (!rowContainer) {
+          rowContainer = document.createElement('div');
+          rowContainer.className = 'product-row';
+          productContainer.appendChild(rowContainer);
+        }
+        rowContainer.appendChild(productBox);
+      }
 
       productBox.appendChild(productImage);
       productBox.appendChild(productText);
-      productContainer.appendChild(productBox);
-    });
+    }
+
+    productContainer.appendChild(row);
+    
   }
 });
